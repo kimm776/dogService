6de8917b7f6256942eac650bab659f7af9c73918
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-08-06 19:32:04 +0900

    init
    init

---
+++ bigDogService/src/main/java/com/dogService/bigDogService/BigDogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dogService.bigDogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDogServiceApplication.class, args);
+	}
+
+}
---
+++ bigDogService/src/test/java/com/dogService/bigDogService/BigDogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogService.bigDogService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
