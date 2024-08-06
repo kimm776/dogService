3d68b5a53e7490dbd3f495cad940549077bc2aed
lmj <mjlee95613@gmail.com>
2024-08-06 21:01:25 +0900

    test conflict

--- dogService/src/main/java/com/stgTeam/dogService/DogServiceApplication.java
+++ dogService/src/main/java/com/stgTeam/dogService/DogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DogServiceApplication {
-//test
+//test s
 	public static void main(String[] args) {
 		SpringApplication.run(DogServiceApplication.class, args);
 	}
