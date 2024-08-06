27a491cb4ea4f905e9d46999765e68370bbc85d3
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-08-06 21:04:15 +0900

    test3

--- dogService/src/main/java/com/stgTeam/dogService/DogServiceApplication.java
+++ dogService/src/main/java/com/stgTeam/dogService/DogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DogServiceApplication {
-//test s
+//test home에서 고쳐
 	public static void main(String[] args) {
 		SpringApplication.run(DogServiceApplication.class, args);
 	}
