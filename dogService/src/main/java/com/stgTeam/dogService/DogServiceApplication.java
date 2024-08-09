bd1f1173e666a9c091b2079265512e070a739ae1
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-08-10 00:58:31 +0900

    프로젝트 디렉토리 생성

--- dogService/src/main/java/com/stgTeam/dogService/DogServiceApplication.java
+++ dogService/src/main/java/com/stgTeam/dogService/DogServiceApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DogServiceApplication {
-//test home에서 고쳐 커밋할게요
-	//shelf해봐라 나는 커밋할테니
 	public static void main(String[] args) {
 		SpringApplication.run(DogServiceApplication.class, args);
 	}
-
 }
